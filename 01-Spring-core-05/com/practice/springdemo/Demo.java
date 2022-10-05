84c8fbe466dfc4cc22709f797409bfb490bb83c9
Manikanta M <59594162+manikanta3566@users.noreply.github.com>
2022-10-05 15:22:06 +0530

    Add files via upload

---
+++ 01-Spring-core-05/com/practice/springdemo/Demo.java
@@ -0,0 +1,8 @@
+package com.practice.springdemo;
+
+public class Demo {
+    public static void main(String[] args) {
+        Coach coach=new CricketCoach();
+        System.out.println(coach.getDailyPracticeTime());
+    }
+}
