23959953b4e144a26b30fa3d054be76945857156
Manikanta M <59594162+manikanta3566@users.noreply.github.com>
2022-11-16 10:48:38 +0530

    Add files via upload

---
+++ Spring-AOP/Spring-AOP-PointCutDeclarations/src/com/practice/springAOP/config/DemoConfig.java
@@ -0,0 +1,12 @@
+package com.practice.springAOP.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+@Configuration
+@EnableAspectJAutoProxy
+@ComponentScan("com.practice.springAOP")
+public class DemoConfig {
+
+}
